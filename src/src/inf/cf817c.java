06858d9282bf4b59aeb6948db09626f4c30e7b67
TheSK653 <sohamkundu653@gmail.com>
2026-07-22 09:14:22 +0530

    feat: Implement solution for cf817c problem

--- src/src/inf/cf817c.java
+++ src/src/inf/cf817c.java
@@ -2,10 +2,33 @@ package inf;
 import java.util.Scanner;
 
 public class cf817c {
-	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
-		int n = scanner.nextInt();
-		int k = scanner.nextInt();
-		
-	}
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        long n = sc.nextLong();
+        long s = sc.nextLong();
+
+        long first = -1;
+        long limit = Math.min(n, s + 162);
+
+        for (long i = s; i <= limit; i++) {
+            long temp = i;
+            long sum = 0;
+            while (temp > 0) {
+                sum += temp % 10;
+                temp /= 10;
+            }
+
+            if (i - sum >= s) {
+                first = i;
+                break;
+            }
+        }
+        if (first == -1) {
+            System.out.println(0);
+        } else {
+            System.out.println(n - first + 1);
+        }
+
+    }
 }
